f4d6b880592fe0902384dc837babab96c920e258
Oleg Larkin <krabique48@gmail.com>
2018-01-30 19:23:30 +0000

    Add stub mailer spec (mailer is yet to be implemented)

---
+++ spec/mailers/application_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# Is required to make the coverage reporter happy, although not really used,
+# since mailer is to be implemented yet
+RSpec.describe ApplicationMailer, type: :mailer do
+end
